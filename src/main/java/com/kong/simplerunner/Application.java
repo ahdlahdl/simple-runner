039cea594e4cb7dbfa4bac809c998e1afd4c02da
kongji <ahdlrhtkk@naver.com>
2018-01-23 15:12:28 +0900

    add RestClientRunner example using KongHttpClient

--- src/main/java/com/kong/simplerunner/Application.java
+++ src/main/java/com/kong/simplerunner/Application.java
@@ -1,5 +1,7 @@
 package com.kong.simplerunner;
 
+import com.kong.simplerunner.service.RestClientRunner;
+import com.kong.simplerunner.service.SimpleRunner;
 import org.apache.commons.lang3.ArrayUtils;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -10,5 +12,7 @@ public class Application {
 	public static void main(String[] args) throws Exception {
 		logger.info("args : " + ArrayUtils.toString(args));
 		
+		SimpleRunner runner = new RestClientRunner();
+		runner.execute(args);
     }
 }
---
+++ src/main/java/com/kong/simplerunner/service/RestClientRunner.java
@@ -0,0 +1,22 @@
+package com.kong.simplerunner.service;
+
+import com.kong.simplerunner.infrastructure.http.KongHttpClient;
+import org.apache.http.client.methods.HttpGet;
+import org.apache.http.client.methods.HttpRequestBase;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class RestClientRunner implements SimpleRunner {
+	private Logger logger = LoggerFactory.getLogger(getClass());
+	
+	@Override
+	public void execute(String[] args) {
+		String proxyHost = null;
+		Integer proxyPort = null;
+		
+		KongHttpClient client = new KongHttpClient(proxyHost, proxyPort);
+		HttpRequestBase http = new HttpGet("https://google.com");
+	
+		logger.info("### result : " + client.execute(http));
+    }
+}
